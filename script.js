d5bbce98baeb11b72261d05973d30f4dbeba0d44
13jaque <jaquelinegidelfonso@gmail.com>
2021-07-24 09:05:04 -0300

    atualização css/html/javascript

--- script.js
+++ script.js
@@ -1,3 +1,73 @@
+function salvarformproduto(){
+
+  if (localStorage.cont) {
+  
+    localStorage.cont = Number(localStorage.cont)+1;
+  
+    } else {
+  
+      localStorage.cont = 1;
+            
+    }
+var nomeprod = document.getElementById("nome").value;
+var emailprod = document.getElementById("emailform").value;
+var telefoneprod = document.getElementById("telefoneform").value;
+var generoprod = document.getElementById("generoproduto").value;
+var tamanhoprod = document.getElementById("tamanhoproduto").value;
+var modeloprod = document.getElementById("modeloproduto").value;
+
+var cadastroproduto = {
+
+  nomeprod,
+  emailprod,
+  telefoneprod,
+  generoprod,
+  tamanhoprod,
+  modeloprod
+
+};
+
+localStorage.setItem(
+  "cadastroproduto_", 
+  JSON.stringify (cadastroproduto), 
+  localStorage.cont, cadastroproduto
+);
+
+}
+
+function salvarformcliente(){
+
+  if (localStorage.cont) {
+  
+    localStorage.cont = Number(localStorage.cont)+1;
+  
+    } else {
+  
+      localStorage.cont = 1;
+            
+    }
+
+var nomecliente = document.getElementById("nomecliente").value;
+var emailcliente = document.getElementById("emailcliente").value;
+var telefonecliente = document.getElementById("telefonecliente").value;
+
+var cadastrocliente = {
+
+  nomecliente,
+  emailcliente,
+  telefonecliente
+
+};
+
+localStorage.setItem(
+  "cadastrocliente_", 
+  JSON.stringify (cadastrocliente), 
+  localStorage.cont, cadastrocliente
+);
+
+}
+
+//popo-up
 $ = function(id) {
     return document.getElementById(id);
   }
@@ -8,3 +78,6 @@ $ = function(id) {
   var hide = function(id) {
       $(id).style.display ='none';
   }
+
+
+
